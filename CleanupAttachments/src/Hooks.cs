c1634dc3af5b27474535c2a1a38e5990b0085016
M'aiq <m.janik1031@gmail.com>
2021-04-25 23:29:54 -0500

    add proj files

---
+++ CleanupAttachments/src/Hooks.cs
@@ -0,0 +1,49 @@
+﻿using FistVR;
+
+namespace CleanupAttachments
+{
+	public class Hooks
+	{
+
+		public Hooks()
+		{
+			Hook();
+		}
+
+		public void Dispose()
+		{
+			Unhook();
+		}
+
+		private void Hook()
+		{
+			On.FistVR.FVRWristMenu.CleanUpScene_Guns += FVRWristMenu_CleanUpScene_Guns;
+		}
+
+		private void Unhook()
+		{
+			On.FistVR.FVRWristMenu.CleanUpScene_Guns -= FVRWristMenu_CleanUpScene_Guns;
+		}
+
+		private void FVRWristMenu_CleanUpScene_Guns(On.FistVR.FVRWristMenu.orig_CleanUpScene_Guns orig, FistVR.FVRWristMenu self)
+		{
+			var confirm = self.askConfirm_CleanupGuns;
+
+			orig(self);
+
+			if (!confirm)
+			{
+				return;
+			}
+
+			FVRFireArmAttachment[] array = UnityEngine.Object.FindObjectsOfType<FVRFireArmAttachment>();
+			for (int i = array.Length - 1; i >= 0; i--)
+			{
+				if (!array[i].IsHeld && array[i].curMount == null && array[i].QuickbeltSlot == null)
+				{
+					UnityEngine.Object.Destroy(array[i].gameObject);
+				}
+			}
+		}
+	}
+}
---
+++ CleanupAttachments/src/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using Deli.Setup;
+
+namespace CleanupAttachments
+{
+	public class Plugin : DeliBehaviour
+	{
+		private readonly Hooks _hooks;
+
+		public Plugin()
+		{
+			_hooks = new Hooks();
+		}
+
+		private void OnDestroy()
+		{
+			_hooks?.Dispose();
+		}
+	}
+}
